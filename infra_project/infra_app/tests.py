db245544270f0026bcdb65c97d8b0bad2ca484f8
angelofwars <angelofwars59@gmail.com>
2023-05-09 20:44:28 +0500

    fix work test

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -9,15 +9,10 @@ class StaticPagesURLTests(TestCase):
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEquals(response.status_code, 200)
 
-        response = self.guest_client.get('/second_page/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-
-        response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
